5e1d55802fc9d9cf41fcb5fac77e8b3653e0d536
zaadie <saadiyafourie@gmail.com>
2019-01-22 17:42:26 +1100

    Colour comparison function creeated to tell the user if they are correct/incorrect

--- colourGame.js
+++ colourGame.js
@@ -10,6 +10,7 @@ var colours = [
 var squares = document.querySelectorAll(".square");
 var pickedColour = colours[3];
 var colourDisplay = document.getElementById("colourDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colourDisplay.textContent = pickedColour;
 
@@ -22,6 +23,14 @@ for(var i = 0; i < squares.length; i++){
 
     //add click listeners to squares
     squares[i].addEventListener("click", function(){
-        alert("clicked a square");
+        //grab colour of clicked square
+        var clickedColour = this.style.background;
+        //compare colour to pickedColour
+        if(clickedColour === pickedColour){
+            messageDisplay.textContent = "Correct";
+        } else {
+            this.style.background="#232323";
+            messageDisplay.textContent = "Try Again";
+        }
     });
 }
